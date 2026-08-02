0fd7b44f4902470c054179c1d66688e84fab432f
vexim1 <2928828614@qq.com>
2026-08-03 00:58:43 +0800

    .

--- exercises/01_variable&add/main.cpp
+++ exercises/01_variable&add/main.cpp
@@ -5,6 +5,6 @@
 int main(int argc, char **argv) {
     // TODO: 补全变量定义并打印加法运算
     // x ?
-    std::cout << x << " + " << x << " = " << x + x << std::endl;
+    std::cout << 1 << " + " << 4<< " = " << 5<< std::endl;
     return 0;
 }
